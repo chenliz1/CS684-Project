104115917d36ce148f91419e59778fe735e560ca
Lizhe Chen <lizhe.chen@mail.utoronto.ca>
2019-12-07 20:46:26 -0500

    update readme

--- README.md
+++ README.md
@@ -1,6 +1,24 @@
 # CS684-Project
-The repo for CS684 Final Project.
+##The repo for CS684 Final Project.
 
-Validation data:
+##Validation data:
 1. Download http://www.cvlibs.net/downloads/depth_devkit.zip
-2. Unzip to ~/data
+2. Unzip depth_selection folder to "CS684-Project/data/"
+
+##Training data:
+1. Download https://s3.eu-central-1.amazonaws.com/avg-kitti/raw_data/2011_09_26_drive_0001/2011_09_26_drive_0001_sync.zip
+2. Download https://s3.eu-central-1.amazonaws.com/avg-kitti/raw_data/2011_09_29_drive_0071/2011_09_29_drive_0071_sync.zip
+3. Unzip. In "2011_09_**" folders, move "2011_09_**_drive_****_sync" folders to "CS684-Project/data/"
+
+##Dataser Preparation:
+1. The files in "CS684-Project/data/" look like this:<br>
+<p>
+ |--- /CS684-Project<br>
+ |------ /data <br>
+ |--------- /2011_**_0001_sync<br>
+ |--------- /2011_**_0071_sync<br>
+ |--------- /depth_selection<br>
+ |--------- datasetCreator.py<br>
+ |--------- read_depth.py<br></p>
+
+2. Under "CS684-Project/data/", run datasetCreator.py.
--- data/datasetCreator.py
+++ data/datasetCreator.py
@@ -1,6 +1,7 @@
 import glob
 import os
 from PIL import Image
+import numpy as np
 
 train_data_folders = ["2011_09_26_drive_0001_sync", "2011_09_29_drive_0071_sync"]
 
@@ -35,23 +36,24 @@ def valImages(img_path, gts_path, target_path):
     target_img_path = os.path.join(target_path, "image/")
     target_gts_path = os.path.join(target_path, "groundtruth/")
 
-    for i in range(len(img_list)):
-        target_name = str(i).zfill(5) + ".jpg"
+    for i in range(3):#len(gts_list)):
+        target_name = str(i).zfill(5)
 
         img = Image.open(os.path.join(img_path, img_list[i]))
         img = img.convert('RGB')
-        img.save(os.path.join(target_img_path, target_name))
+        img.save(os.path.join(target_img_path, target_name + ".jpg"))
 
         gts = Image.open(os.path.join(gts_path, gts_list[i]))
-        gts = gts.convert('L')
-        gts.save(os.path.join(target_gts_path, target_name))
+        gts.save(os.path.join(target_gts_path, target_name + ".png"))
 
 
 def trainImages(train_data_folders, target_path):
     counter = 0
     for folder in train_data_folders:
-        img_path = os.path.join(folder, "image_02/")
+
+        img_path = os.path.join(folder, "image_02/data")
         gts_path = os.path.join(folder, "proj_depth/groundtruth/image_02/")
+
         img_list = list(glob.glob1(img_path, "*.png"))
         img_list.sort()
         gts_list = list(glob.glob1(gts_path, "*.png"))
@@ -60,19 +62,19 @@ def trainImages(train_data_folders, target_path):
         target_img_path = os.path.join(target_path, "image/")
         target_gts_path = os.path.join(target_path, "groundtruth/")
 
-        for i in range(len(img_list)):
-            target_name = str(counter).zfill(5) + ".jpg"
+        for i in range(len(gts_list)):
+            target_name = str(counter).zfill(5)
 
-            img = Image.open(os.path.join(img_path, img_list[i]))
+            img = Image.open(os.path.join(img_path, gts_list[i]))
             img = img.convert('RGB')
-            img.save(os.path.join(target_img_path, target_name))
+            img.save(os.path.join(target_img_path, target_name + ".jpg"))
 
             gts = Image.open(os.path.join(gts_path, gts_list[i]))
             gts = gts.convert('L')
-            gts.save(os.path.join(target_gts_path, target_name))
+            gts.save(os.path.join(target_gts_path, target_name + ".png"))
             counter += 1
 
 
 makeDirs()
-trainImages(val_img_path, val_gts_path, target_val_path)
-
+trainImages(train_data_folders, target_train_path)
+valImages(val_img_path, val_gts_path, target_val_path)
