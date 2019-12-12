f3b4b81c04c8c64097617090bd40e3902b3503df
Caesarzhang <chzft333@gmail.com>
2019-12-11 21:58:52 -0500

    feat: finished dataloader

--- data/dataLoader.py
+++ data/dataLoader.py
@@ -10,6 +10,7 @@ from PIL import Image  # using pillow-simd for increased speed
 import torch
 import torch.utils.data as data
 from torchvision import transforms
+import torchvision.transforms.functional as tF
 
 def pil_loader(path):
     # open path as file to avoid ResourceWarning
@@ -17,26 +18,107 @@ def pil_loader(path):
     with open(path, 'rb') as f:
         with Image.open(f) as img:
             return img.convert('RGB')
+        
+class JointRandomFlip(object):
+    def __call__(self, L, R):
+        if np.random.random_sample()>0.5:
+            return (tF.hflip(R),tF.hflip(L))
+        return (L,R)
+    
+class JointRandomColorAug(object):
+
+    def __init__(self,gamma=(0.8,1.2),brightness=(0.5,2.0),color_shift=(0.8,1.2)):
+        self.gamma = gamma
+        self.brightness = brightness
+        self.color_shift = color_shift
+
+    def __call__(self, L, R):
+        if  np.random.random_sample()>0.5:
+            
+            random_gamma = np.random.uniform(*self.gamma)
+            L_aug = L ** random_gamma
+            R_aug = R ** random_gamma
+
+            random_brightness = np.random.uniform(*self.brightness)
+            L_aug = L_aug * random_brightness
+            R_aug = R_aug * random_brightness
+
+            random_colors = np.random.uniform(self.color_shift[0],self.color_shift[1], 3)
+            for i in range(3):
+                L_aug[i, :, :] *= random_colors[i]
+                R_aug[i, :, :] *= random_colors[i]
+
+            # saturate
+            L_aug = torch.clamp(L_aug, 0, 1)
+            R_aug = torch.clamp(R_aug, 0, 1)
+
+            return L_aug, R_aug
+
+        else:
+            return L, R
+
+class JointToTensor(object):
+    def __call__(self, L, R):
+        return tF.to_tensor(L),tF.to_tensor(R)
+    
+class JointToImage(object):
+    def __call__(self, L, R):
+        return transforms.ToPILImage()(L),transforms.ToPILImage()(R)
+    
+    
+class JointCompose(object):
+    def __init__(self, transforms):
+        """
+        params: 
+           transforms (list) : list of transforms
+        """
+        self.transforms = transforms
+
+    # We override the __call__ function such that this class can be
+    # called as a function i.e. JointCompose(transforms)(img, target)
+    # Such classes are known as "functors"
+    def __call__(self, img, target):
+        """
+        params:
+            img (PIL.Image)    : input image
+            target (PIL.Image) : ground truth label 
+        """
+        assert img.size == target.size
+        for t in self.transforms:
+            img, target = t(img, target)
+        return img, target
+
+
+train_joint_transform = JointCompose([JointRandomFlip(),JointToTensor(),JointRandomColorAug()])
+
 
 class TwoViewDataset(data.Dataset):
-    def __init__(self, data_path, resize_shape=(1242, 375), is_train=False):
+    
+    def __init__(self, 
+                 data_path,
+                 resize_shape=(1242, 375), 
+                 is_train=False,
+                 transforms=None,
+                 sanity_check=None):
         super(TwoViewDataset, self).__init__()
         self.data_path = data_path
 
         self.interp = Image.ANTIALIAS
         self.resize_shape = resize_shape
         self.is_train = is_train
-
+        self.transforms=transforms
         self.loader = pil_loader
-        self.to_tensor = transforms.ToTensor()
+        
         if is_train:
             self.imgR_folder = os.path.join(data_path, "train", "image_right")
             self.imgL_folder = os.path.join(data_path, "train", "image_left")
         else:
             self.imgR_folder = os.path.join(data_path, "val", "image_right")
             self.imgL_folder = os.path.join(data_path, "val", "image_left")
-
-
+        
+        
+        self.imgR=[os.path.join(self.imgR_folder, x) for x in os.listdir(self.imgR_folder)]
+        self.imgL=[os.path.join(self.imgL_folder, x) for x in os.listdir(self.imgL_folder)]
 
     def get_color(self, path, do_flip):
         color = self.loader(path)
@@ -45,39 +127,15 @@ class TwoViewDataset(data.Dataset):
         return self.to_tensor(color)
 
 
-    #
-    # def get_depth(self, path, do_flip):
-    #     depth_png = Image.open(path)
-    #
-    #     depth_png = depth_png.resize(self.resize_shape, Image.NEAREST)
-    #     if do_flip:
-    #         depth_png = depth_png.transpose(Image.FLIP_LEFT_RIGHT)
-    #     depth =  np.array(depth_png, dtype = int)
-    #
-    #     assert (np.max(depth_png) > 255)
-    #     depth = np.array(depth_png).astype(np.float) / 256.
-    #     depth[depth_png == 0] = -1.
-    #
-    #
-    #     return torch.from_numpy(depth.astype(np.float32))
-
     def __len__(self):
         return len(list(glob.glob1(self.imgL_folder, "*.jpg")))
 
     def __getitem__(self, index):
-
-        imgL_fn = os.path.join(self.imgL_folder, str(index).zfill(5) + ".jpg")
-        imgR_fn = os.path.join(self.imgR_folder, str(index).zfill(5) + ".jpg")
-
-        do_flip = self.is_train and random.random() > 0.5
-
-        colorL = self.get_color(imgL_fn, do_flip)
-        colorR = self.get_color(imgR_fn, do_flip)
-        # depth = self.get_depth(gts_fn, do_flip)
-
+        colorR=Image.open(self.imgR[index]).convert('RGB')
+        colorL=Image.open(self.imgL[index]).convert('RGB')
+        
+        if self.transforms is not None:
+            colorR, colorL = self.transforms(colorR, colorL)
         return colorL, colorR
-
-
-
 
 
