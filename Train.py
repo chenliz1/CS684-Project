db3f6f015ed81c18fa69a6440fc31b7a4101500f
gghiro <58644045+gghiro@users.noreply.github.com>
2019-12-12 17:50:27 -0500

    Update Train.py

--- Train.py
+++ Train.py
@@ -19,28 +19,79 @@ class Train(object):
         optimizer = torch.optim.SGD(net.parameters(), lr=0.001, weight_decay=1e-5, momentum=0.5, nesterov=False)
         return optimizer
     
-    def train(self, train_loader, net, loss_graph):
-        train_loader = self.train_loader
-        net = self.net
-        loss_graph = self.loss_graph
-        optimizer = self.get_optimizer()
+    def train(self):
+        losses = []; val_losses = []
+        min_loss = np.inf; min_val_loss = np.inf 
+
+        sum_val_loss = 0.0 
         
-        for i, data in enumerate(train_loader):
+        self.model.eval()
+        
+        for data in self.val_loader:
+            #Computing disparities and finding validation loss
+            left = data['left_image']
+            right = data['right_image']
+            left = left.cuda()
+            right = right.cuda()
+            disps = self.model(left)
+            #loss = self.loss_function(disps, [left, right])
+            
+            
+           
+            val_losses.append(loss.item())
+            sum_val_loss += loss.item()
+            i += 1
+            
+        tot_val_loss = (sum_val_loss/ (self.val_n_img * self.args.batch_size))
+        print('Initial Validiation Loss:', tot_val_loss)
+        
+        
+        
+        f = open("Val_train_loss.txt", "x"); i = 0
+        for e in range(self.args.epochs):
+            time0 = time.time()
+            total_train_loss = 0.0
+            self.model.train()
+            
+            for data in self.loader:
+                left = data['left_image']
+                right = data['right_image']
+                left = left.cuda()
+                right = right.cuda()
+            
+                self.optimizer.zero_grad()
+                disps = self.model(left)
+                loss = self.loss_function(disps, [left, right])
+                loss.backward()
+                self.optimizer.step()
+                losses.append(loss.item())
+                total_train_loss += loss.item()
 
-            inputs, masks = data
+            sum_val_loss = 0.0
+            self.model.eval()
+            for data in self.val_loader:
+                left = data['left_image']
+                right = data['right_image']
+                left = left.cuda()
+                right = right.cuda()
+                disps = self.model(left)
+                loss = self.loss_function(disps, [left, right])
+                val_losses.append(loss.item())
+                sum_val_loss += loss.item()
 
-            if USE_GPU:
-                inputs = inputs.cuda()
-                net = net.cuda()
-                masks = masks.cuda()
+            # Estimate loss per image
+            total_train_loss = total_train_loss/(self.n_img * self.args.batch_size)
+            tot_val_loss = (sum_val_loss/ (self.val_n_img * self.args.batch_size))
+            f.write("Val Loss: ", tot_val_loss, "  ", "Train Loss: ", total_train_loss)
+            
+            if total_train_loss < min_val_loss:
+                self.saveParams('params.pkl')
+                min_val_loss = tot_val_loss
+                print('Model_saved')
 
-            optimizer.zero_grad()
-            main_loss = net(inputs, gts = masks)
-            loss_graph.append(main_loss.item())
-            main_loss.backward()
-            optimizer.step()
-
-        return main_loss
+        f.close()
+        print ('Min loss:', min_loss)
+        self.saveParams('params.pkl')
 
 
 
