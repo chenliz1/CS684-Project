9a2f72fc91e77f12467bd155245f52e26a45d42c
caesarzhang@zju.edu.cn <caesarzhang@zju.edu.cn>
2019-12-16 15:26:18 -0500

    fix vgg model

--- models/model_vgg.py
+++ models/model_vgg.py
@@ -56,32 +56,27 @@ class VGGDispModel(nn.Module):
         # model_dict.update(pretrained_dict)
         # vgg16.load_state_dict(model_dict)
 
-        filters = [64, 128, 256, 512]
+        filters = [64, 128, 256, 512, 512]
         layers = list(vgg16_bn.children())[0]
-        self.conv1 = layers[0]
-        self.layer0 = nn.Sequential(*layers[1:6])
+        self.layer0 = nn.Sequential(*layers[0:6])
         self.layer1 = nn.Sequential(*layers[6:13])
         self.layer2 = nn.Sequential(*layers[13:23])
         self.layer3 = nn.Sequential(*layers[23:33])
         self.layer4 = nn.Sequential(*layers[33:43])
 
-
-        self.upconv6 = upconv(filters[3], 512, 3, 2)
-        self.iconv6 = iconv(filters[2] + 512, 512, 3, 1)
-
-        self.upconv5 = upconv(512, 256, 3, 2)
-        self.iconv5 = iconv(filters[1] + 256, 256, 3, 1)
+        self.upconv5 = upconv(filters[4], 256, 3, 2)
+        self.iconv5 = iconv(filters[3] + 256, 256, 3, 1)
 
         self.upconv4 = upconv(256, 128, 3, 2)
-        self.iconv4 = iconv(filters[0] + 128, 128, 3, 1)
+        self.iconv4 = iconv(filters[2] + 128, 128, 3, 1)
         self.disp4_layer = get_disp(128)
 
-        self.upconv3 = upconv(128, 64, 3, 1) #
-        self.iconv3 = iconv(64 + 64 + 2, 64, 3, 1)
+        self.upconv3 = upconv(128, 64, 3, 2) #
+        self.iconv3 = iconv(filters[1] + 64 + 2, 64, 3, 1)
         self.disp3_layer = get_disp(64)
 
         self.upconv2 = upconv(64, 32, 3, 2)
-        self.iconv2 = iconv(64 + 32 + 2, 32, 3, 1)
+        self.iconv2 = iconv(filters[0] + 32 + 2, 32, 3, 1)
         self.disp2_layer = get_disp(32)
 
         self.upconv1 = upconv(32, 16, 3, 2)
@@ -94,54 +89,53 @@ class VGGDispModel(nn.Module):
 
     def forward(self, x):
         # encoder
-        x_conv1=self.conv1(x)
-        x0 = self.layer0(x_conv1)
+        x0 = self.layer0(x)
         x1 = self.layer1(x0)
         x2 = self.layer2(x1)
         x3 = self.layer3(x2)
         x4 = self.layer4(x3)
         # skips
-        skip1 = x_conv1
-        skip2 = x0
-        skip3 = x1
-        skip4 = x2
-        skip5 = x3
-        # print(skip4.size())
+        skip1 = x0
+        skip2 = x1
+        skip3 = x2
+        skip4 = x3
 
         # decoder
-        upconv6 = self.upconv6(x4)
-        concat6 = torch.cat((upconv6, skip5), 1)
-        iconv6 = self.iconv6(concat6)
 
-        upconv5 = self.upconv5(iconv6)
-        # print(upconv5.size())
-        concat5 = torch.cat((upconv5, skip4), 1)
+        upconv5 = self.upconv5(x4)
+        concat5 = torch.cat((upconv5, skip4), 1) # 512
         iconv5 = self.iconv5(concat5)
 
         upconv4 = self.upconv4(iconv5)
-        concat4 = torch.cat((upconv4, skip3), 1)
+        concat4 = torch.cat((upconv4, skip3), 1) # 256
         iconv4 = self.iconv4(concat4)
         self.disp4 = self.disp4_layer(iconv4)
-        self.udisp4 = nn.functional.interpolate(self.disp4, scale_factor=1, mode='bilinear', align_corners=True)
+        self.udisp4 = nn.functional.interpolate(self.disp4, scale_factor=2, mode='bilinear', align_corners=True)
         self.disp4 = nn.functional.interpolate(self.disp4, scale_factor=0.5, mode='bilinear', align_corners=True)
 
         upconv3 = self.upconv3(iconv4)
-        concat3 = torch.cat((upconv3, skip2, self.udisp4), 1)
+
+        concat3 = torch.cat((upconv3, skip2, self.udisp4), 1) #128
         iconv3 = self.iconv3(concat3)
         self.disp3 = self.disp3_layer(iconv3)
         self.udisp3 = nn.functional.interpolate(self.disp3, scale_factor=2, mode='bilinear', align_corners=True)
+        self.disp3 = nn.functional.interpolate(self.disp3, scale_factor=0.5, mode='bilinear', align_corners=True)
 
         upconv2 = self.upconv2(iconv3)
-        concat2 = torch.cat((upconv2, skip1, self.udisp3), 1)
+        concat2 = torch.cat((upconv2, skip1, self.udisp3), 1) #64
         iconv2 = self.iconv2(concat2)
         self.disp2 = self.disp2_layer(iconv2)
         self.udisp2 = nn.functional.interpolate(self.disp2, scale_factor=2, mode='bilinear', align_corners=True)
+        self.disp2 = nn.functional.interpolate(self.disp2, scale_factor=0.5, mode='bilinear', align_corners=True)
 
         upconv1 = self.upconv1(iconv2)
         concat1 = torch.cat((upconv1, self.udisp2), 1)
         iconv1 = self.iconv1(concat1)
         self.disp1 = self.disp1_layer(iconv1)
-        
+        self.disp1 = nn.functional.interpolate(self.disp1, scale_factor=0.5, mode='bilinear', align_corners=True)
+
+
+
         return self.disp1, self.disp2, self.disp3, self.disp4
 
 
